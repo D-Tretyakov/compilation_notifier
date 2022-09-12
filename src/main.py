b3c033c51fedbd9559f72d9a6ccd0e6d8145eec5
D-Tretyakov <tretyakov.daniil.education@gmail.com>
2022-09-12 03:07:01 +0300

    moved code to src dir

--- src/main.py
+++ src/main.py
@@ -1,12 +1,11 @@
 from bot import Bot
 
-TOKEN = 'TOKEN'
-USER_ID = "12345"
+TOKEN = ''
+USER_ID = ''
 
 def main():
     bot = Bot(TOKEN, USER_ID)
     bot.notify()
-    # bot.get_update()
 
 
 if __name__ == '__main__':
